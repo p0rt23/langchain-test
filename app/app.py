13270cd2215407c82bd5164b3c9192657268d724
Mark Smith <msmith4@gmail.com>
2023-07-26 21:04:51 +0000

    Restored changes

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import sys
+from langchain.llm import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+model_id = "google/flan-t5-large" 
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+llm = HuggingFacePipeline( pipeline = (
+    "text2text-generation",
+    model = model,
+    tokenizer = tokenizer,
+    max_length = 100
+))
+ 
+template = """Question: {question}
+ 
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+while True:
+    question = input("Your question (exit to quit): ")
+
+    if question == "exit":
+        sys.exit()
+    else:
+        print(llm_chain.run(question))
